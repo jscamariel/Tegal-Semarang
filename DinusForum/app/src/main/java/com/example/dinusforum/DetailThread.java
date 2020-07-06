29a3dba6d079dd8245c419b87f6337111e6eff44
geraldolazarus <61725661+geraldolazarus@users.noreply.github.com>
2020-07-06 23:58:07 +0700

    Android App

---
+++ DinusForum/app/src/main/java/com/example/dinusforum/DetailThread.java
@@ -0,0 +1,14 @@
+package com.example.dinusforum;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class DetailThread extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_detail_thread);
+    }
+}
--- DinusForum/app/src/main/java/com/example/dinusforum/MyAccountFragment.java
+++ DinusForum/app/src/main/java/com/example/dinusforum/MyAccountFragment.java
@@ -11,47 +11,17 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.ImageButton;
-import android.widget.LinearLayout;
 
 
 /**
  * A simple {@link Fragment} subclass.
  */
-public class MyAccountFragment extends Fragment {
-    Activity context;
-    private LinearLayout datadiri;
-    private LinearLayout password;
+public class MyAccountFragment extends Fragment{
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        context = getActivity();
         return inflater.inflate(R.layout.fragment_my_account, container, false);
-    }
-
-    @Override
-    public void onStart() {
-        super.onStart();
-        datadiri = context.findViewById(R.id.datadiri);
-        password = context.findViewById(R.id.password);
-
-
-        datadiri.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                startActivity(new Intent(context, DataDiri.class));
-            }
-
-
-        });
-
-        password.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                startActivity(new Intent(context, Password.class));
-            }
-        });
-
     }
 }
